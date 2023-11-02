0aad9dd5bca3d65254b0a2a0568a60152083fff6
Carlos Miranda <36993908+carlosmirandaf@users.noreply.github.com>
2023-11-02 14:00:22 -0300

    Atualizando o código graph_deezer_romenia.py

--- requisito_02/source/graph_deezer_romenia.py
+++ requisito_02/source/graph_deezer_romenia.py
@@ -3,9 +3,11 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 import csv
 
 G = nx.Graph()
+fig, ax = plt.subplots(figsize=(9,9))
 
 nodes_left = set()
 nodes_right = set()
@@ -63,10 +65,13 @@ avg_degree_connectivity = nx.average_degree_connectivity(G)
 degrees, avg_connectivities = zip(*avg_degree_connectivity.items())
 
 # Crie um gráfico de dispersão
-plt.scatter(degrees, avg_connectivities, alpha=0.5, label='Scatter Plot')
+plt.scatter(degrees, avg_connectivities, s=60, alpha=0.5, edgecolors = 'k', label='Scatter Plot')
+b, a = np.polyfit(degrees, avg_connectivities, deg=1)
+xseq = np.linspace(min(degrees), max(degrees), num=100)
+ax.plot( xseq, a + b * xseq, color='red', lw=2.5)
 
 # Adicione uma linha de assortatividade no gráfico
-plt.axhline(assortativity, color='red', linestyle='--', label='Assortativity')
+#plt.axhline(assortativity, color='red', linestyle='--', label='Assortativity')
 
 # Defina rótulos dos eixos e legenda
 plt.xlabel('Node Degree')
